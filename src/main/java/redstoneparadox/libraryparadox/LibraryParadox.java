48635f1fdedd0c765ac6b19c470e6a4141f65206
RedstoneParadox <spinjitzu00@gmail.com>
2018-09-27 12:23:24 -0700

    More stuff

--- src/main/java/redstoneparadox/libraryparadox/LibraryParadox.java
+++ src/main/java/redstoneparadox/libraryparadox/LibraryParadox.java
@@ -17,7 +17,7 @@ import redstoneparadox.libraryparadox.signals.Switchboard;
 public class LibraryParadox {
 
     public static final String MOD_ID = "libraryparadox";
-    public static final String MOD_NAME = "Library Paradox";
+    public static final String MOD_NAME = "LibraryParadox";
     public static final String VERSION = "1.0.0";
 
     /**
@@ -37,6 +37,7 @@ public class LibraryParadox {
      */
     @Mod.EventHandler
     public void preinit(FMLPreInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(new LPEventHandler());
         logger = event.getModLog();
         switchboard = new Switchboard();
