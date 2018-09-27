f28628103fe3dd4fd18d668ff784fc804d13d262
RedstoneParadox <spinjitzu00@gmail.com>
2018-09-27 13:14:56 -0700

    Finally got it to load

--- src/main/java/redstoneparadox/libraryparadox/LibraryParadox.java
+++ src/main/java/redstoneparadox/libraryparadox/LibraryParadox.java
@@ -18,7 +18,7 @@ import redstoneparadox.libraryparadox.signals.Switchboard;
 public class LibraryParadox {
 
     public static final String MOD_ID = "libraryparadox";
-    public static final String MOD_NAME = "LibraryParadox";
+    public static final String MOD_NAME = "Library Paradox";
     public static final String VERSION = "1.0.0";
 
     /**
